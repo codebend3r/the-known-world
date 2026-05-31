1c0493a3404ce94845f228a76e3fe2e6b9398bfc
CJ Rivas <cj.rivas.dev@gmail.com>
2026-05-31 19:53:45 -0400

    TKW: convert `ComingSoonPage` module from CSS to SCSS
    - rename `components/ComingSoonPage.module.css` to `.module.scss`
    - update `ComingSoonPage.tsx` import to the `.scss` path

--- components/ComingSoonPage.tsx
+++ components/ComingSoonPage.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import styles from "@/components/ComingSoonPage.module.css";
+import styles from "@/components/ComingSoonPage.module.scss";
 
 export function ComingSoonPage({ title }: { title: string }) {
   return (
