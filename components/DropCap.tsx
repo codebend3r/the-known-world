8507945f3a174742d59c7694e965d114fe3ff028
CJ Rivas <cj.rivas.dev@gmail.com>
2026-05-31 19:54:08 -0400

    TKW: convert `DropCap` module from CSS to SCSS
    - rename `components/DropCap.module.css` to `.module.scss`
    - update `DropCap.tsx` import to the `.scss` path

--- components/DropCap.tsx
+++ components/DropCap.tsx
@@ -1,5 +1,5 @@
 import type { ReactNode } from "react";
-import styles from "@/components/DropCap.module.css";
+import styles from "@/components/DropCap.module.scss";
 
 export function DropCap({ children }: { children: ReactNode }) {
   return <p className={styles.dropCap}>{children}</p>;
